c6ddb37e8a86db748e5f8cdfd3a25c4d64c5da53
Антон Лучкин <luchkin1988@gmail.com>
2020-02-28 17:39:49 +0200

    hw-js-mod-5

---
+++ js/task-03.js
@@ -0,0 +1,44 @@
+// Напиши класс Storage, который будет создавать объекты для управления складом товаров.
+//  При вызове будет получать один аргумент - начальный массив товаров, и записывать его в свойство items.
+
+class Storage {
+  constructor(items) {
+    this.items = items;
+  }
+  getItems() {
+    console.table(this.items);
+  }
+
+  addItem(item) {
+    storage.items.push(item);
+  }
+
+  removeItem(item) { 
+    let itemIndex = storage.items.indexOf(item);
+    if (itemIndex != -1) {
+        storage.items.splice(itemIndex, 1);
+    }
+  }
+
+}
+
+// Добавь методы класса:
+
+// getItems() - возвращает массив текущих товаров
+// addItem(item) - получает новый товар и добавляет его к текущим
+// removeItem(item) - получет товар и, если он есть, удаляет его из текущих
+const storage = new Storage([
+  "Нанитоиды",
+  "Пролонгер",
+  "Железные жупи",
+  "Антигравитатор"
+]);
+
+const items = storage.getItems();
+console.table(items); // [ "Нанитоиды", "Пролонгер", "Железные жупи", "Антигравитатор" ]
+
+storage.addItem("Дроид");
+console.table(storage.items); // [ "Нанитоиды", "Пролонгер", "Железные жупи", "Антигравитатор", "Дроид" ]
+
+storage.removeItem("Пролонгер");
+console.table(storage.items); // [ "Нанитоиды", "Железные жупи", "Антигравитатор", "Дроид" ]
